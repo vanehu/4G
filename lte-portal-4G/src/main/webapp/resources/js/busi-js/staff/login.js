3d4d0bc5498b07155d7c20853d06b6af5d170b17
liuteng <liuteng@03d26769-fb22-2544-a5c1-c2b74650ae0b>
2015-10-13 07:00:37 +0000

    23633 cookie清空--huangjj
    git-svn-id: http://192.168.1.20:8000/svn/17portal/ec-portal/LTE_PUBLISH@61986 03d26769-fb22-2544-a5c1-c2b74650ae0b

--- lte-portal-4G/src/main/webapp/resources/js/busi-js/staff/login.js
+++ lte-portal-4G/src/main/webapp/resources/js/busi-js/staff/login.js
@@ -8,6 +8,19 @@ CommonUtils.regNamespace("staff", "login");
  * 员工登录
  */
 staff.login = (function($) {
+	//update by huangjj3 清空cookie #23633
+	var cookies = document.cookie.split(";");
+
+	for (var i = 0; i < cookies.length; i++) {
+		 var cookie = cookies[i];
+		 var eqPos = cookie.indexOf("=");
+		 var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+		 //document.cookie = name + new Date();
+		 var nameT = name.toUpperCase();
+		 if(nameT.indexOf("LTEA10")>=0){
+			 $.cookie(name, null, { path: '/' });
+		 }
+	 }
 
 	//记录staffId
 	var staffId =null;
