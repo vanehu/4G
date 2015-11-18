244096d0e25e6c0d759fe236cdb6c05a445a0209
zhangyu15 <zhangyu15@03d26769-fb22-2544-a5c1-c2b74650ae0b>
2015-11-18 12:07:14 +0000

    Redmine #48858 修复购手机流程返回终端列表页空白--yangms
    git-svn-id: http://192.168.1.20:8000/svn/17portal/ec-portal/LTE_PUBLISH@63275 03d26769-fb22-2544-a5c1-c2b74650ae0b

--- lte-portal-4G/src/main/webapp/resources/js/app-js/busi-js/common/common.js
+++ lte-portal-4G/src/main/webapp/resources/js/app-js/busi-js/common/common.js
@@ -105,7 +105,7 @@ common = (function($) {
 		}
 		var arr=new Array(1);
 		arr[0]="";
-		MyPlugin.relocationCust(arr,
+		MyPlugin.closeWebview(arr,
             function(result) {
             },
             function(error) {
@@ -120,7 +120,7 @@ common = (function($) {
 		}
 		var arr=new Array(1);
 		arr[0]="";
-		MyPlugin.closeWebview(arr,
+		MyPlugin.relocationCust(arr,
             function(result) {
             },
             function(error) {
@@ -175,6 +175,7 @@ common = (function($) {
 	
 	//客户端调用此方法返回到上一页 1 为prepare页面  2为order-content（填单）页面 3为order-confirm（订单确认和收银台）页面 4为order-print（打印）页面
 	var _callReturnBack=function(){
+		$.unecOverlay();//网络出现故障或手机出现故障时按返回关闭“加载中”提示框
 		if(OrderInfo.actionFlag==4||OrderInfo.actionFlag==8){//客户新增和修改
 			_callCloseWebview();
 			return;
