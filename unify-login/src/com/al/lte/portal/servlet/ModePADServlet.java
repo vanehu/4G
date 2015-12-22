1beac70530ba4dfebb903f8bec20a77b33d6626d
liuteng <liuteng@03d26769-fb22-2544-a5c1-c2b74650ae0b>
2015-12-22 07:47:21 +0000

    #61830-能力开放单点登录在UnifyLogin对能力开放的版本单独分省切换AB版本--wangyu10
    git-svn-id: http://192.168.1.20:8000/svn/17portal/ec-portal/LTE_PUBLISH@64493 03d26769-fb22-2544-a5c1-c2b74650ae0b

--- unify-login/src/com/al/lte/portal/servlet/ModePADServlet.java
+++ unify-login/src/com/al/lte/portal/servlet/ModePADServlet.java
@@ -39,43 +39,51 @@ public class ModePADServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType( "text/html;charset=utf-8" ) ;  //设置响应页面字符编码   
-		String accessToken = request.getParameter("accessToken");//获取到的凭证	
-		Map<String, String[]> map=request.getParameterMap();
-		boolean flag = true;
-		StringBuffer queryString = new StringBuffer();
-		for (String key : map.keySet()) {
-			String[] values = map.get(key);
-			for (int i = 0; i < values.length; i++) {
-				String value = values[i];
-				if (flag) {
-					flag = false;
-					queryString.append(key).append("=").append(value);
-				} else {
-					queryString.append("&").append(key).append("=").append(value);
-				}
-			}
-			
-		}
-		//update by huangjj3 清除客户端4层生成的cookie
-		DelCookie.delCookie(response, "LTEA10", null, "/",  request);
-		String jmAccessToken = AESUtils.decryptToString(accessToken, Const.TOKEN_KEY);
-		String[] accessTokenStr = jmAccessToken.split("#");
-		
-		String province = Config.getAreaName(accessTokenStr[1]);
-		String Port = Config.getProvVersion(province);
-		String httpconfig = "";
-		if("81".equals(Port) || "82".equals(Port)){
-			httpconfig = "http";
-		}else if("83".equals(Port) || "84".equals(Port) || "85".equals(Port)){
-			httpconfig = "https";
-		}else{
-			httpconfig = "http";
-		}
-		String uri = request.getRequestURI().replaceAll("ltePortal", "provPortal");
-		String url = httpconfig + "://" + Config.getIpconfig(request) + ":"
-			+ Port + uri +"?"+ queryString.toString()
-			+ "&prov=" + province;
 		try {
+			String accessToken = request.getParameter("accessToken");//获取到的凭证	
+			Map<String, String[]> map=request.getParameterMap();
+			boolean flag = true;
+			StringBuffer queryString = new StringBuffer();
+			for (String key : map.keySet()) {
+				String[] values = map.get(key);
+				for (int i = 0; i < values.length; i++) {
+					String value = values[i];
+					if (flag) {
+						flag = false;
+						queryString.append(key).append("=").append(value);
+					} else {
+						queryString.append("&").append(key).append("=").append(value);
+					}
+				}
+				
+			}
+			//update by huangjj3 清除客户端4层生成的cookie
+			DelCookie.delCookie(response, "LTEA10", null, "/",  request);
+			String jmAccessToken = "";
+			try{
+				jmAccessToken = AESUtils.decryptToString(accessToken, Const.TOKEN_KEY);
+				if(jmAccessToken==null){
+					response.getWriter().print("{resultCode:\"-1\",resultMsg:\"参数解密异常\"}");
+					return;
+				}
+			}catch(Exception e){
+				
+			}
+			String[] accessTokenStr = jmAccessToken.split("#");
+			String province = Config.getAreaName(accessTokenStr[1]);
+			String Port = Config.getProvVersion("NL-"+province);
+			String httpconfig = "";
+			if("81".equals(Port) || "82".equals(Port)){
+				httpconfig = "http";
+			}else if("83".equals(Port) || "84".equals(Port) || "85".equals(Port)){
+				httpconfig = "https";
+			}else{
+				httpconfig = "http";
+			}
+			String uri = request.getRequestURI().replaceAll("ltePortal", "provPortal");
+			String url = httpconfig + "://" + Config.getIpconfig(request) + ":"
+				+ Port + uri +"?"+ queryString.toString()
+				+ "&prov=" + province;
 			response.sendRedirect(url);
 		} catch (IOException e) {
 			e.printStackTrace();
