697360f0cad48003503299f78a7ccb264ffb2fe7
zhangyu15 <zhangyu15@03d26769-fb22-2544-a5c1-c2b74650ae0b>
2016-04-19 08:07:05 +0000

    优化 Redmine #77405 去掉4G功能同时去掉爱玩爱听爱看,转4G后不会默认开通【翼支付】，若要添加翼支付功能，需手工点选--wufeng
    git-svn-id: http://192.168.1.20:8000/svn/17portal/ec-portal/LTE_PUBLISH@68384 03d26769-fb22-2544-a5c1-c2b74650ae0b

--- lte-portal-4G/src/main/webapp/resources/js/pad-js/op-js/order/attachOffer.js
+++ lte-portal-4G/src/main/webapp/resources/js/pad-js/op-js/order/attachOffer.js
@@ -2104,12 +2104,14 @@ AttachOffer = (function() {
 				dependOffer : {  //存放互斥依赖列表
 					dependOffers : [],
 					offerGrpInfos : []
-				}
+				},
+				optDependOffer : []
 		};
 		if(result!=""){
 			var exclude = result.offerSpec.exclude;
 			var depend = result.offerSpec.depend;
-			
+			var defaultOffer=result.offerSpec.defaultList;
+			var optDependOffer = result.offerSpec.optDependList;
 			//解析可选包互斥依赖组
 			if(ec.util.isArray(exclude)){
 				for (var i = 0; i < exclude.length; i++) {
@@ -2131,6 +2133,8 @@ AttachOffer = (function() {
 					}
 				}
 			}
+			
+			
 			if(depend!=undefined && ec.util.isArray(depend.offerInfos)){
 				for (var i = 0; i < depend.offerInfos.length; i++) {	
 					content += "需要开通： " + depend.offerInfos[i].offerSpecName + "<br>";
@@ -2150,7 +2154,23 @@ AttachOffer = (function() {
 					}
 				}
 			}
+			//解析可选包默选组
+			if(ec.util.isArray(defaultOffer)){
+				for (var i = 0; i < defaultOffer.length; i++) {	
+					content += "需要开通： " + defaultOffer[i].offerSpecName + "<br>";
+					param.defaultOffer.push(defaultOffer[i].offerSpecId);
+				}	
+			}
+			if (optDependOffer != undefined && ec.util.isArray(optDependOffer)) {
+				for (var i = 0; i < optDependOffer.length; i++) {
+					content += "可选订购：" + '<input id="'+optDependOffer[i].offerSpecId+'" type="checkbox" value="'+optDependOffer[i].offerSpecId+'"/>' + optDependOffer[i].offerSpecName + "<br>";
+					param.optDependOffer.push(optDependOffer[i].offerSpecId);
+				}
+			}
+			
 		}
+		
+		
 		var serContent=_servExDepReByRoleObjs(prodId,offerSpecId);//查询销售品构成成员的依赖互斥以及连带
 		content=content+serContent;
 		if(content==""){ //没有互斥依赖
@@ -4235,6 +4255,7 @@ AttachOffer = (function() {
 		paramObj.excludeServ=[];//初始化
 		paramObj.dependServ=[];//初始化
 		paramObj.relatedServ=[];//初始化
+		paramObj.offerListServ=[];//初始化
 		var globContent="";
 		$.each(newSpec.offerRoles,function(){
 			$.each(this.roleObjs,function(){
@@ -4286,6 +4307,7 @@ AttachOffer = (function() {
 		var servExclude = result.servSpec.exclude; //互斥
 		var servDepend = result.servSpec.depend; //依赖
 		var servRelated = result.servSpec.related; //连带
+		var servOfferList = result.servSpec.offerList; //带出的可选包
 		var content = "";
 		
 		//解析功能产品互斥
@@ -4327,6 +4349,22 @@ AttachOffer = (function() {
 				}
 			});
 		}
+		//解析带出的可选包，获取功能产品订购依赖互斥的接口返回的带出可选包拼接成字符串
+		if(ec.util.isArray(servOfferList)){
+			if(servOfferList.length>0){
+				content += "需要订购：   <br>";
+				$.each(servOfferList,function(){
+					if(this.ifDault===0){
+						content += '<input id="check_open_'+prodId+'_'+this.offerSpecId +'" type="checkbox" checked="checked" disabled="disabled">'+this.offerSpecName+'<br>'; 
+					}else{
+						content += '<input id="check_open_'+prodId+'_'+this.offerSpecId +'" type="checkbox" checked="checked">'+this.offerSpecName+'<br>'; 
+					}
+//					content += "需要订购：   " + this.offerSpecName + "<br>";
+					paramObj.offerListServ.push(this);
+				});
+			}
+		}
+
 		return content;
 	};
 	
