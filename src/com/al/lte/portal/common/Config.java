8d0079500a533d0260a9472de7cd99c3f594dfb5
zhangyu15 <zhangyu15@03d26769-fb22-2544-a5c1-c2b74650ae0b>
2016-12-13 13:13:04 +0000

    #1069477 ECS-集约CRM电渠业务融合改造------统一门户登陆页面： 增加‘电渠门户’--需求撤销代码回退--张宇
    git-svn-id: http://192.168.1.20:8000/svn/17portal/ec-portal/LTE_PUBLISH/unify-login@73207 03d26769-fb22-2544-a5c1-c2b74650ae0b

--- src/com/al/lte/portal/common/Config.java
+++ src/com/al/lte/portal/common/Config.java
@@ -136,7 +136,6 @@ public class Config {
 			case 846: areaName = "hainan"; break;
 			case 823: areaName = "heilongjiang"; break;
 			case 899: areaName = "xuni"; break;
-			case 898: areaName = "ess"; break;
 		}
 		return areaName;
 	}
