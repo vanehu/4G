96dba6c3297fc4f13b6b099b92ab1ef2ad398678
huangjj3 <huangjj3@03d26769-fb22-2544-a5c1-c2b74650ae0b>
2017-03-15 07:55:40 +0000

    redmine1307621一证五号能力开发版本
    git-svn-id: http://192.168.1.20:8000/svn/17portal/ec-portal/LTE_PUBLISH@74787 03d26769-fb22-2544-a5c1-c2b74650ae0b

--- lte-portal-4G/src/main/webapp/resources/js/pad-js/busi-js/cust/cust.js
+++ lte-portal-4G/src/main/webapp/resources/js/pad-js/busi-js/cust/cust.js
@@ -952,7 +952,7 @@ order.cust = (function(){
             if (ec.util.isObj(result)) {
             	if (ec.util.isObj(result)) {
             		ec.util.mapPut(OrderInfo.oneCardFiveNO.usedNum, _getCustInfo415Flag(inParam), result.usedNum);
-            		if(parseInt(result.usedNum)>=5){
+            		if(parseInt(result.usedNum)>=5 && OrderInfo.actionFlag ==1){
                 		$.alert("提示", "工信部要求支撑全国实名制一证五卡验证,一个用户证件下不能有超过5个号码！");
                 	}else if(parseInt(result.usedNum) <5 && OrderInfo.oneCardFiveNum.length<=0){
                 		checkResult=true;
--- lte-portal-4G/src/main/webapp/resources/js/pc-js/busi-js/cust/cust.js
+++ lte-portal-4G/src/main/webapp/resources/js/pc-js/busi-js/cust/cust.js
@@ -3205,7 +3205,7 @@ order.cust = (function(){
             if (ec.util.isObj(result)) {
             	if (ec.util.isObj(result)) {
             		ec.util.mapPut(OrderInfo.oneCardFiveNO.usedNum, _getCustInfo415Flag(inParam), result.usedNum);
-            		if(parseInt(result.usedNum)>=5){
+            		if(parseInt(result.usedNum)>=5 && OrderInfo.actionFlag ==0){
                 		$.alert("提示", "工信部要求支撑全国实名制一证五卡验证,一个用户证件下不能有超过5个号码！");
                 		checkResult = false;
                 	}else if(parseInt(result.usedNum) <5 && OrderInfo.oneCardFiveNum.length<=0){
