fbada348360dd9d1c6150486be1c2351bdfb401c
yangms <yangms@03d26769-fb22-2544-a5c1-c2b74650ae0b>
2017-03-24 04:51:20 +0000

    #1307655-翼销售接入欠费查询、充值缴费功能-----营业门户（APP）-zhoukl
    git-svn-id: http://192.168.1.20:8000/svn/17portal/ec-portal/LTE_PUBLISH@75157 03d26769-fb22-2544-a5c1-c2b74650ae0b

--- ecsp-core/src/main/java/com/ailk/ecsp/intf/httpclient/HttpClient.java
+++ ecsp-core/src/main/java/com/ailk/ecsp/intf/httpclient/HttpClient.java
@@ -5,6 +5,8 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -14,6 +16,11 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.ailk.ecsp.intf.webservice.WSConfig;
+import com.ailk.ecsp.util.IConstant;
+import com.ailk.ecsp.util.SoapUtil;
+import com.al.ec.serviceplatform.client.ResultCode;
 
 public class HttpClient {
 
@@ -51,6 +58,25 @@ public class HttpClient {
 			response.put("result", resp);
 		}
 		
+		return response;
+	}
+	
+	/**
+	 * callByHttpClient方法重载
+	 * @param message 请求报文
+	 * @param url 请求地址
+	 * @param config 配置
+	 * @return 返回xml转换成的map
+	 */
+	public Map<String, Object> callByHttpClient(String message, String url, WSConfig config) {
+		Map<String, Object> response = new HashMap<String, Object>();
+		String resp = sendRequest(url, message);
+		if (StringUtils.isBlank(resp)) {
+			response.put("resultCode", ResultCode.R_FAILURE);
+		}else {
+			// 解析xml成map
+			response = parseResponseContent(resp, config);
+		}
 		return response;
 	}
 	
@@ -93,5 +119,45 @@ public class HttpClient {
 		return responseMsg;
 	}
 	
-	
+	public Map<String, Object> parseResponseContent(String content, WSConfig config){
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		Map<String, Object> responseMap = null;
+		try {
+			content = StringEscapeUtils.unescapeXml(content);
+			if (StringUtils.isNotBlank(content)) {
+				// 清除xml头
+				content = content.replaceAll("(<\\?xml.+?>)", "");
+				content = content.replaceAll("\t","");//制表符
+				content = content.replaceAll("\r","");//回车符
+				content = content.replaceAll("\n","");//换行符
+			}
+			logger.debug(":return:"+content);
+			if(IConstant.CON_OUT_PARAM_TYPE_TO_XML.equals(config.getOutParamType())){
+				returnMap.put("result", content);
+			}else{
+				responseMap = SoapUtil.xmlToMap(content, config.getOutParamType());
+				returnMap.put("result", responseMap);
+				returnMap.put("resultParam", content);
+				//returnMap.put("resIntfMsg", content);
+				if(MapUtils.isEmpty(responseMap)){
+					returnMap.put("resultCode", "WS-2002");
+					returnMap.put("resultMsg", "回参转换失败,回参："+content);
+					return returnMap;
+				}
+			}
+			if (config.isSiebel()) {
+				String sessionToken = SoapUtil.parseSessionToken(content);
+				if (StringUtils.isNotBlank(sessionToken)){
+					returnMap.put("SessionToken",SoapUtil.parseSessionToken(content));
+				}
+			}
+		} catch (Exception e) {
+			responseMap = null;
+			logger.error("","回参转换失败,回参："+content);
+			logger.error("",e);
+		}
+		returnMap.put("resultCode", "POR-0000");
+		returnMap.put("resultMsg", "OK!");
+		return returnMap;
+	}
 }
