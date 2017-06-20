edd8e4a67d37e10d535a4f300445687a4abc972b
xuxu <Beckham_07@126.com>
2017-06-20 17:07:05 +0800

    1528122 营销推荐测试问题修改

--- lte-portal-4G/src/main/webapp/resources/js/busi-js/cust/cust.js
+++ lte-portal-4G/src/main/webapp/resources/js/busi-js/cust/cust.js
@@ -4770,10 +4770,10 @@ order.cust = (function(){
 		});
 	};
 	
-	var _saveMktContactResult =function(objId,activityId,id){
+	var _saveMktContactResult =function(mktmodelCode,activityId,id){
     	var param = {
 	        areaId : OrderInfo.staff.areaId,
-	        mktmodelCode : objId,
+	        mktmodelCode : mktmodelCode,
 	        contactChlId : OrderInfo.staff.channelId,
 	        contactStaff : OrderInfo.staff.staffId,
 	        pushType : "3000",
