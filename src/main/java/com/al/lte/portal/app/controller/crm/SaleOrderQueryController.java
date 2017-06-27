34360deb2e66300bd0b3871339dfa7fac177a4a7
xujian2 <xujian2@522f099e-abda-49d8-82c2-8e4bd9fe781f>
2017-06-27 09:55:39 +0000

    SONAR问题修改
    git-svn-id: http://svn.asiainfo.org/svn/17portal/ec-portal/LTE/%E6%BA%90%E4%BB%A3%E7%A0%81-New/LTE/app-portal-4G@77311 522f099e-abda-49d8-82c2-8e4bd9fe781f

--- src/main/java/com/al/lte/portal/app/controller/crm/SaleOrderQueryController.java
+++ src/main/java/com/al/lte/portal/app/controller/crm/SaleOrderQueryController.java
@@ -1,21 +1,11 @@
 package com.al.lte.portal.app.controller.crm;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.collections.MapUtils;
-import org.apache.commons.io.filefilter.FalseFileFilter;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -25,31 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.al.ec.entity.StaffInfo;
 import com.al.ec.serviceplatform.client.ResultCode;
 import com.al.ecs.common.entity.JsonResponse;
-import com.al.ecs.common.entity.Switch;
-import com.al.ecs.common.util.JsonUtil;
-import com.al.ecs.common.util.MDA;
 import com.al.ecs.common.util.PropertiesUtils;
 import com.al.ecs.common.web.ServletUtils;
 import com.al.ecs.exception.AuthorityException;
-import com.al.ecs.exception.BusinessException;
 import com.al.ecs.exception.ErrorCode;
-import com.al.ecs.exception.InterfaceException;
 import com.al.ecs.exception.ResultConstant;
 import com.al.ecs.spring.annotation.log.LogOperatorAnn;
 import com.al.ecs.spring.annotation.session.AuthorityValid;
-import com.al.ecs.spring.annotation.session.SessionValid;
 import com.al.ecs.spring.controller.BaseController;
 import com.al.lte.portal.bmo.crm.CommonBmo;
 import com.al.lte.portal.bmo.crm.OrderBmo;
-import com.al.lte.portal.bmo.portal.NoticeBmo;
-import com.al.lte.portal.common.CommonMethods;
-import com.al.lte.portal.common.Const;
 import com.al.lte.portal.common.EhcacheUtil;
 import com.al.lte.portal.common.SysConstant;
-import com.al.lte.portal.core.DataRepository;
 import com.al.lte.portal.model.SessionStaff;
 
 /**
@@ -91,8 +70,6 @@ public class SaleOrderQueryController extends BaseController {
     public String main(Model model,HttpSession session,@LogOperatorAnn String flowNum) throws AuthorityException {
 		model.addAttribute("current", EhcacheUtil.getCurrentPath(session,"saleorder/queryorderlist"));
 		
-		SessionStaff sessionStaff = (SessionStaff) ServletUtils.getSessionAttribute(super.getRequest(),
-				SysConstant.SESSION_KEY_LOGIN_STAFF);
 		
 		return "/app/saleOrderQuery/saleOrderQuery";		
 	}
