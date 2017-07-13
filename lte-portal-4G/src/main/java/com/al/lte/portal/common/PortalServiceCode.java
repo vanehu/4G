47593afc95273c6ab409a8b2d6f3019a67811171
liuteng <liuteng@asiainfo.com>
2017-07-13 10:05:41 +0800

    1566939 跨省一证五卡处理流程优化  ----  报表功能 ----  营业门户[1.添加接口名称定义]

--- lte-portal-4G/src/main/java/com/al/lte/portal/common/PortalServiceCode.java
+++ lte-portal-4G/src/main/java/com/al/lte/portal/common/PortalServiceCode.java
@@ -1239,7 +1239,7 @@ public final class PortalServiceCode {
     /**
      * 跨省一证五号订单报表统计接口
      */
-    public static final String CERT_NUMBER_REPORT = "biz-service/ intf.detailService/queryCollectionOrderItemCount";
+    public static final String CERT_NUMBER_REPORT = "biz-service/intf.detailService/queryCollectionOrderItemCount";
 
 }
 
