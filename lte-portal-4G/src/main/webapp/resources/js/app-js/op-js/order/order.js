dab11fd6381e71a5070e0940c39daac13ea776f1
liuteng <liuteng@asiainfo.com>
2017-07-19 10:03:21 +0800

    1502517 关于天翼云盘（个人版、企业版）产品IT需求-----（不调用一证五号校验）-----营业门户（能力开放）

--- lte-portal-4G/src/main/webapp/resources/js/app-js/op-js/order/order.js
+++ lte-portal-4G/src/main/webapp/resources/js/app-js/op-js/order/order.js
@@ -277,7 +277,19 @@ order.service = (function(){
 		if(!offerSpec){
 			return;
 		}
-		if(OrderInfo.actionFlag == 2){ //套餐变更
+
+        if (needCheck(offerSpec)) {
+            if (OrderInfo.actionFlag == 2) {  //套餐变更不做校验
+                cust.preCheckCertNumberRelQueryOnly(cust.getCustInfo415());//调用一证五号校验接口获取已有的数量
+            } else if (!CacheData.isGov(OrderInfo.cust.custId == "-1" ? OrderInfo.boCustIdentities.identidiesTypeCd : OrderInfo.cust.identityCd)) {//政企客户新装不调用一证五号校验
+                //一证五号校验
+                if (!cust.preCheckCertNumberRel("-1", cust.getCustInfo415())) {
+                    return;
+                }
+            }
+        }
+
+        if(OrderInfo.actionFlag == 2){ //套餐变更
 			var url=contextPath+"/app/order/queryFeeType";
 			$.ecOverlay("<strong>正在查询是否判断付费类型的服务中,请稍后....</strong>");
 			var response = $.callServiceAsJsonGet(url,param);	
@@ -738,7 +750,31 @@ order.service = (function(){
 			$.alert("提示","套餐详细加载失败，请稍后再试！");
 		}
 	};
-	return {
+
+    /**
+     * 判断某个销售品是否需要调用一证五号校验，如果为以下三种，需求调用一证五号校验，其它情况都不调用校验
+     * 235010000 移动电话（仅含本地语音）
+     * 280000000 天翼宽带-无线数据卡
+     * 280000025 智机通
+     * @param offerSpec 销售品实例数据
+     */
+    function needCheck(offerSpec) {
+        var needCheck = false;
+        if (ec.util.isObj(offerSpec) && ec.util.isObj(offerSpec.offerRoles) && offerSpec.offerRoles.length > 0) {
+            $.each(offerSpec.offerRoles, function () {
+                if (ec.util.isObj(this.roleObjs) && this.roleObjs.length > 0) {
+                    $.each(this.roleObjs, function () {
+                        if (this.objId == "235010000" || this.objId == "280000000" || this.objId == "280000025") {
+                            needCheck = true;
+                        }
+                    })
+                }
+            });
+        }
+        return needCheck;
+    }
+
+    return {
 		btnBack					:			_btnBack,
 		buyService 				:			_buyService,
 		queryApConfig			:			_queryApConfig,
