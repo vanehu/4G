77661b8636c7b73977bafd028b36bbcc1d727327
zhangyu15 <zhangyu15@asiainfo.con>
2017-07-26 10:44:41 +0800

    Revert "Merge branch 'huangjj' into develop"
    This reverts commit e38dc712cdf3fc0b615bab5c59993824d64b5826, reversing
    changes made to d64c489bba625838aa7354934b3ece8c3a142ad9.

--- lte-portal-4G/src/main/java/com/al/lte/portal/controller/crm/OrderProdModifyController.java
+++ lte-portal-4G/src/main/java/com/al/lte/portal/controller/crm/OrderProdModifyController.java
@@ -764,11 +764,11 @@ public class OrderProdModifyController extends BaseController {
 		paramMap.put("custId", sessionStaff.getCustId());
 		 HttpSession session = request.getSession();
 		String areaId = (String) session.getAttribute("pointareaId");
-		Map<String, Object> areaInfo = CommonMethods.getAreaInfo(areaId);
-		if("1300".equals(MapUtils.getString(areaInfo, "regionType"))){
-			paramMap.put("areaId", areaId); 
-		} else {
-			paramMap.put("areaId", areaId.substring(0, 5) + "00"); 
+		if(!"".equals(areaId) && areaId !=null){
+			paramMap.put("areaId", areaId.substring(0, 5) + "00");
+		}else{
+			 areaId = sessionStaff.getAreaId();
+			 paramMap.put("areaId",areaId.substring(0, 5) + "00");
 		}
 		String comPreFlag = propertiesUtils.getMessage("COMANDFXPRECHECK_"+areaId.substring(0, 3));
 		try {
