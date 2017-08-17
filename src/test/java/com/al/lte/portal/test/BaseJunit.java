3e34d72e9a38d39f1c6e87ed97aede36b55ba516
xujian2 <xujian2@522f099e-abda-49d8-82c2-8e4bd9fe781f>
2017-08-17 09:29:08 +0000

    修改配置文件路径
    git-svn-id: http://svn.asiainfo.org/svn/17portal/ec-portal/LTE/%E6%BA%90%E4%BB%A3%E7%A0%81-New/LTE/app-portal-4G@78061 522f099e-abda-49d8-82c2-8e4bd9fe781f

--- src/test/java/com/al/lte/portal/test/BaseJunit.java
+++ src/test/java/com/al/lte/portal/test/BaseJunit.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath*:/spring/portal-context-junit.xml" })
+@ContextConfiguration(locations = { "classpath*:/spring/portal-context.xml" })
 public abstract class BaseJunit {
 
 }
