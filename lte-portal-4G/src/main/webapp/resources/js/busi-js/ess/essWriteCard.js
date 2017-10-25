27e0cb3d45f5faa65d5171cfb43b8a61164667a9
yangbin5 <yangbin5@asiainfo.com>
2017-10-25 14:39:36 +0800

    优化（开发） #1944180-融合ecs写卡页面查询增加必选条件限制

--- lte-portal-4G/src/main/webapp/resources/js/busi-js/ess/essWriteCard.js
+++ lte-portal-4G/src/main/webapp/resources/js/busi-js/ess/essWriteCard.js
@@ -45,6 +45,29 @@ ess.writeCard = (function() {
 		var orderStatus = ec.util.defaultStr($("#p_orderStatus").val());
 		var startDate = ec.util.defaultStr($("#p_startDate").val());
 		var endDate = ec.util.defaultStr($("#p_endDate").val());
+		if(extCustOrderId == "" && consignee == "" && accNbr == "" && orderStatus == "" && startDate == "" && endDate == ""){
+			$.alert("提示","请输入查询条件！");
+			return;
+		}
+		if(startDate != "" || endDate != ""){
+			if(startDate == ""){
+				$.alert("提示","请选择 开始时间！");
+				return;
+			}
+			if(endDate == ""){
+				var nowDate = Date.now();
+				endDate = DateUtil.Format('yyyy-MM-dd', nowDate);
+			}
+			try {
+				var startDate_temp = new Date(startDate);
+				var endDate_temp = new Date(endDate);
+				if((endDate_temp.getTime() - startDate_temp.getTime())/1000/60/60/24 > 31){
+					$.alert("提示","请将查询时间段缩小至一个月以内 !");
+					return;
+				}
+			} catch (e) {
+			}
+		}
 		var param = {
 			extCustOrderId : extCustOrderId,
 			consignee : consignee,
