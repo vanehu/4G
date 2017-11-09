f0cb2d7fe542d57332af40ab6603bde04dc08d69
zhangyu15 <zhangyu15@asiainfo.com>
2017-11-09 11:16:49 +0800

    Complete - task : Redmine#1781175关于增加对销售品订购次数进行判断和限制的需求 ---- 营业门户（WEB）【方案变更，二次开发】

--- lte-portal-4G/src/main/webapp/resources/js/app-js/op-js/newmember/attachOffer.js
+++ lte-portal-4G/src/main/webapp/resources/js/app-js/op-js/newmember/attachOffer.js
@@ -1037,9 +1037,12 @@ AttachOffer = (function() {
 			return;
 		}
 		
-		var checkOrderTimesResult = check.offer.orderTimes(newSpec);
-		if(!checkOrderTimesResult){
-			return;
+		var offer = CacheData.getOfferBySpecId(prodId,offerSpecId); //从已订购数据中找
+		if(ec.util.isObj(offer)){
+			var checkOrderTimesResult = check.offer.orderTimes(offer, null);
+			if(!checkOrderTimesResult){
+				return;
+			}
 		}
 		
 		var content = CacheData.getOfferProdStr(prodId,newSpec,0);
@@ -3506,6 +3509,11 @@ AttachOffer = (function() {
 				return;
 			}
 			if(flag==1 && offer!=undefined){
+				//校验销售品已订购、可订购次数
+				var checkResult = check.offer.orderTimes(offer, nums);
+				if(!checkResult){
+					return;
+				}
 				if(offer.orderCount>nums){//退订附属销售品
 					if(!ec.util.isArray(offer.offerMemberInfos)){//销售品实例查询	
 						var param = {
